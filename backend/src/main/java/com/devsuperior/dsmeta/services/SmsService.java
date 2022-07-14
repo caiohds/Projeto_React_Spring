3abc2cca24b32d80b283473881f860b03a6b2451
caiohds <caiohds2020@gmail.com>
2022-07-14 18:13:51 -0300

    alguns updates

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,14 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SmsService {
